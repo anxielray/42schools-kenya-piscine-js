082d1f3857d8c897bc7f995c4e9206d7005c1d45
anxielray <anxielworld@gmil.com>
2024-09-30 19:42:21 +0300

    [Add]:Add solution to the Isaac Newton's ame on the second law of motion

--- physics.js
+++ physics.js
@@ -4,22 +4,9 @@ function getAcceleration(params) {
     params.length == 0 ||
     params.m == null ||
     params.f == null ||
-    params.Δv == null ||
-    params.Δt == null ||
-    params.t == null ||
-    params.d == null ||
     typeof params.m !== "number" ||
     typeof params.f !== "number" ||
-    typeof params.Δv !== "number" ||
-    typeof params.Δt !== "number" ||
-    params.t <= 0 ||
-    params.d <= 0 ||
-    params.m <= 0 ||
-    params.f <= 0 ||
-    params.Δv <= 0 ||
-    params.Δt <= 0 ||
-    params.t <= 0 ||
-    params.d <= 0
+    params.f <= 0
   ) {
     return "impossible";
   }
