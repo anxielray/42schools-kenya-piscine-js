27e57d68deb74ad83a6cec737ac31eecd22a45cf
anxielray <anxielworld@gmil.com>
2024-10-01 00:50:57 +0300

    [Add]:Add solution to the block chain

--- block-chain.js
+++ block-chain.js
@@ -3,7 +3,7 @@
 //     [...str].reduce((h, c) => (h = (h << 5) - h + c.charCodeAt(0)) & h, 0) >>> 0
 //   ).toString(36);
 
-function blockChain(data, prev = { index: 0, hash: "0" }) {
+function blockChain(data, prev = { index: 0, hash: '0' }) {
   const hash = hashCode(`${prev.index + 1}${prev.hash}${JSON.stringify(data)}`);
 
   return {
@@ -17,27 +17,27 @@ function blockChain(data, prev = { index: 0, hash: "0" }) {
 
 // Test the blockChain function
 const first = blockChain({ a: 1 });
-// console.log(first.index); // -> 1
-// console.log(first.data); // -> { a: 1 }
-// console.log(first.prev); // -> { index: 0, hash: '0' }
-// console.log(first.hash); // Should output '1103f27'
-// console.log(hashCode('10{"a":1}')); // -> '1103f27'
+console.log(first.index); // -> 1
+console.log(first.data); // -> { a: 1 }
+console.log(first.prev); // -> { index: 0, hash: '0' }
+console.log(first.hash); // Should output '1103f27'
+console.log(hashCode('10{"a":1}')); // -> '1103f27'
 
-const second = first.chain({ hello: "world" });
-// console.log(second.hash); // -> '18drvvc'
-// console.log(hashCode('21103f27{"hello":"world"}')); // -> '18drvvc'
+const second = first.chain({ hello: 'world' });
+console.log(second.hash); // -> '18drvvc'
+console.log(hashCode('21103f27{"hello":"world"}')); // -> '18drvvc'
 
 const chain = second
   .chain({ value: 4455 })
-  .chain({ some: "data" })
-  .chain({ cool: "stuff" });
+  .chain({ some: 'data' })
+  .chain({ cool: 'stuff' });
 
 const fork = second
   .chain({ value: 335 })
-  .chain({ some: "data" })
-  .chain({ cool: "stuff" });
+  .chain({ some: 'data' })
+  .chain({ cool: 'stuff' });
 
-// console.log(chain.hash); // -> '1qr3qfs'
-// console.log(fork.hash); // -> '1x9gsc1'
-// console.log(chain.index); // -> 5
-// console.log(fork.index); // -> 5
+console.log(chain.hash); // -> '1qr3qfs'
+console.log(fork.hash); // -> '1x9gsc1'
+console.log(chain.index); // -> 5
+console.log(fork.index); // -> 5
