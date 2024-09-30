8e2668bdceaa3a1f4e9c7349d1fa3c727288cb20
anxielray <anxielworld@gmil.com>
2024-09-30 11:00:59 +0300

    [Add]:Add solution to the declarations

--- declarations.js
+++ declarations.js
@@ -1,1 +1,21 @@
 const escapeStr = 'Anxile\'s, "World", /!\\';
+const arr = [4, 2];
+const obj = {
+  str: "Hello, World!",
+  num: 10,
+  bool: true,
+  undef: undefined,
+};
+const nested = {
+  arr: [4, undefined, "2"],
+  obj: {
+    str: "Hello, World!",
+    num: 10,
+    bool: true,
+    undef: undefined,
+  },
+};
+Object.freeze(nested);
+Object.freeze(nested.arr);
+Object.freeze(nested.obj);
+console.log(nested);
