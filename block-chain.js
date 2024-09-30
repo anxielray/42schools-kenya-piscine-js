f61624d3939dddd5720cff4b1536c79dcb1d7b62
anxielray <anxielworld@gmil.com>
2024-10-01 01:03:56 +0300

    [Add]:Add solution to the block chain

--- block-chain.js
+++ block-chain.js
@@ -1,43 +1,21 @@
-// const hashCode = (str) =>
-//   (
-//     [...str].reduce((h, c) => (h = (h << 5) - h + c.charCodeAt(0)) & h, 0) >>> 0
-//   ).toString(36);
-
-function blockChain(data, prev = { index: 0, hash: '0' }) {
-  const hash = hashCode(`${prev.index + 1}${prev.hash}${JSON.stringify(data)}`);
-
-  return {
-    index: prev.index + 1,
-    hash,
-    data,
-    prev,
-    chain: (newData) => blockChain(newData, { index: prev.index + 1, hash }), // Use updated reference
-  };
+function hashCode(str) {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    let char = str.charCodeAt(i);
+    hash = (hash << 5) - hash + char;
+    hash = hash | 0; //hash & hash;
+  }
+  return hash.toString(); //Math.abs(hash);
 }
 
-// Test the blockChain function
-const first = blockChain({ a: 1 });
-console.log(first.index); // -> 1
-console.log(first.data); // -> { a: 1 }
-console.log(first.prev); // -> { index: 0, hash: '0' }
-console.log(first.hash); // Should output '1103f27'
-console.log(hashCode('10{"a":1}')); // -> '1103f27'
-
-const second = first.chain({ hello: 'world' });
-console.log(second.hash); // -> '18drvvc'
-console.log(hashCode('21103f27{"hello":"world"}')); // -> '18drvvc'
-
-const chain = second
-  .chain({ value: 4455 })
-  .chain({ some: 'data' })
-  .chain({ cool: 'stuff' });
-
-const fork = second
-  .chain({ value: 335 })
-  .chain({ some: 'data' })
-  .chain({ cool: 'stuff' });
-
-console.log(chain.hash); // -> '1qr3qfs'
-console.log(fork.hash); // -> '1x9gsc1'
-console.log(chain.index); // -> 5
-console.log(fork.index); // -> 5
+function blockChain(data, prev = { index: 0, hash: "0" }) {
+  const block = {
+    index: index + 1,
+    data: data,
+    prev: prev,
+    get hash() {
+      return hashCode(this.indes + this.prev.hash + JSON.stringify(this.data));
+    },
+  };
+  return block;
+}
