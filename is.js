544bfd89d96cccb8c0b96070ddec7dc99d06377c
anxielray <anxielworld@gmil.com>
2024-09-30 16:06:38 +0300

    [Add]:Add solution to the is truthy and daa ta types

--- is.js
+++ is.js
@@ -1,33 +1,43 @@
-  is.num =  function (value) {
-    return typeof value === "number";
-  },
-  is.nan = function (value) {
-    return value !== value;
-  },
-  is.str = function (value) {
-    return typeof value === "string";
-  },
-  is.bool = function (value) {
-    return typeof value === "boolean";
-  },
-  is.undef = function (value) {
-    return value === undefined;
-  },
-  is.def = function (value) {
-    return value !== undefined;
-  },
-  is.arr = function(value) {
-    return Array.isArray(value);
-  };
-  is.obj = function (value) {
-    return value !== null && typeof value === "object";
-  },
-  is.fun = function (value) {
-    return typeof value === "function";
-  },
-  is.truthy = function (value) {
-    return !!value;
-  },
-  is.falsy = function (value) {
-    return !value;
-  };
+is.num = function (value) {
+  return typeof value === "number";
+};
+
+is.nan = function (value) {
+  return value !== value;
+};
+
+is.str = function (value) {
+  return typeof value === "string";
+};
+
+is.bool = function (value) {
+  return typeof value === "boolean";
+};
+
+is.undef = function (value) {
+  return value === undefined;
+};
+
+is.def = function (value) {
+  return value !== undefined;
+};
+
+is.arr = function (value) {
+  return Array.isArray(value);
+};
+
+is.obj = function (value) {
+  return value !== null && typeof value === "object";
+};
+
+is.fun = function (value) {
+  return typeof value === "function";
+};
+
+is.truthy = function (value) {
+  return !!value;
+};
+
+is.falsy = function (value) {
+  return !value;
+};
