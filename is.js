aadd29601eed7063af2acc715e7d1377f6de66c2
anxielray <anxielworld@gmil.com>
2024-09-30 15:47:55 +0300

    [Add]:Add solution to the is truthy and daa ta types

--- is.js
+++ is.js
@@ -1,31 +1,33 @@
   is.num =  function (value) {
     return typeof value === "number";
   },
-  is.nan= function (value) {
+  is.nan = function (value) {
     return value !== value;
   },
-  is.str= function (value) {
+  is.str = function (value) {
     return typeof value === "string";
   },
-  is.bool= function (value) {
+  is.bool = function (value) {
     return typeof value === "boolean";
   },
-  is.undef= function (value) {
+  is.undef = function (value) {
     return value === undefined;
   },
-  is.def= function (value) {
+  is.def = function (value) {
     return value !== undefined;
   },
-  is.arr= Array.isArray,
-  is.obj= function (value) {
+  is.arr = function(value) {
+    return Array.isArray(value);
+  };
+  is.obj = function (value) {
     return value !== null && typeof value === "object";
   },
-  is.fun= function (value) {
+  is.fun = function (value) {
     return typeof value === "function";
   },
-  is.truthy= function (value) {
+  is.truthy = function (value) {
     return !!value;
   },
-  is.falsy= function (value) {
+  is.falsy = function (value) {
     return !value;
   };
