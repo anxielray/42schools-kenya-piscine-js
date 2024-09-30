b35de821d17a70ec964074df3da77cddb84fec33
anxielray <anxielworld@gmil.com>
2024-09-30 16:12:34 +0300

    [Add]:Add solution to the is truthy and data types

--- is.js
+++ is.js
@@ -1,43 +1,21 @@
-is.num = function (value) {
-  return typeof value === "number";
-};
+is.num = (value) => typeof value === "number";
 
-is.nan = function (value) {
-  return value !== value;
-};
+is.nan = (value) => Number.isNaN(value);
 
-is.str = function (value) {
-  return typeof value === "string";
-};
+is.str = (value) => typeof value === "string";
 
-is.bool = function (value) {
-  return typeof value === "boolean";
-};
+is.bool = (value) => typeof value === "boolean";
 
-is.undef = function (value) {
-  return value === undefined;
-};
+is.undef = (value) => typeof value === "undefined";
 
-is.def = function (value) {
-  return value !== undefined;
-};
+is.def = (value) => typeof value !== "undefined";
 
-is.arr = function (value) {
-  return Array.isArray(value);
-};
+is.arr = (value) => Array.isArray(value);
 
-is.obj = function (value) {
-  return value !== null && typeof value === "object";
-};
+is.obj = (value) => value !== null && typeof value === "object";
 
-is.fun = function (value) {
-  return typeof value === "function";
-};
+is.fun = (value) => typeof value === "function";
 
-is.truthy = function (value) {
-  return !!value;
-};
+is.truthy = (value) => !!value === true;
 
-is.falsy = function (value) {
-  return !value;
-};
+is.falsy = (value) => !!value === false;
