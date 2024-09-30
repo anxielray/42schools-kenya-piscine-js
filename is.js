abb2db267b608e5d2875980d6cda7e66e519f8d4
anxielray <anxielworld@gmil.com>
2024-09-30 17:24:08 +0300

    [Add]:Add solution to the is truthy and data types

--- is.js
+++ is.js
@@ -1,51 +1,11 @@
-const is = {};
-is.num = function (value) {
-  if (typeof value === is.nan(value)) {
-    return false;
-  }
-  return typeof value === "number" && !is.nan(value);
-};
-
-is.nan = function (value) {
-  return Number.isNaN(value);
-};
-
-is.str = function (value) {
-  return typeof value === "string";
-};
-
-is.bool = function (value) {
-  return typeof value === "boolean";
-};
-
-is.undef = function (value) {
-  return value === undefined;
-};
-
-is.def = function (value) {
-  return value !== undefined;
-};
-
-is.arr = function (value) {
-  return Array.isArray(value);
-};
-
-is.obj = function (value) {
-  return value !== null && typeof value === "object" && !is.arr(value);
-};
-
-is.fun = function (value) {
-  return typeof value === "function";
-};
-
-is.truthy = function (value) {
-  return !!value;
-};
-
-is.falsy = function (value) {
-  return !is.truthy(value);
-};
-
-is.null = function (value) {
-  return value === null;
-};
+is.num = value => typeof value === 'number' && !is.nan(value);
+is.nan = value => Number.isNaN(value);
+is.str = value => typeof value === 'string';
+is.bool = value => typeof value === 'boolean';
+is.undef = value => typeof value === 'undefined';
+is.def = value => typeof value !== 'undefined';
+is.arr = value => Array.isArray(value);
+is.obj = value => value !== null && typeof value === 'object';
+is.fun = value => typeof value === 'function';
+is.truthy = value => !!value;
+is.falsy = value => !value;
