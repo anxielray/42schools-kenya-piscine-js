fc55d1e7bc7f2370b259dbeba9a445786cfabafe
anxielray <anxielworld@gmil.com>
2024-09-30 19:49:59 +0300

    [Add]:Add solution to the Isaac Newton's ame on the second law of motion

--- physics.js
+++ physics.js
@@ -1,18 +1,47 @@
 function getAcceleration(params) {
+//   if (
+//     params == null ||
+//     typeof params !== "object" ||
+//     typeof params.m !== "number" ||
+//     params.m <= 0
+//   ) {
+//     return "impossible";
+//   }
+
+  const accelerations = [];
+
+  if (typeof params.f === "number") {
+    const aFromForce = params.f / params.m;
+    accelerations.push(aFromForce);
+  }
+
   if (
-    params == null ||
-    params.length == 0 ||
-    params.m == null ||
-    params.f == null ||
-    typeof params.m !== "number" ||
-    typeof params.f !== "number" ||
-    params.f <= 0
+    typeof params.Δv === "number" &&
+    typeof params.Δt === "number" &&
+    params.Δt > 0
   ) {
+    const aFromDeltaV = params.Δv / params.Δt;
+    // accelerations.push(aFromDeltaV);
+    return aFromDeltaV
+  }
+
+  if (
+    typeof params.d === "number" &&
+    typeof params.t === "number" &&
+    params.t > 0
+  ) {
+    const aFromDistance = (2 * params.d) / params.t ** 2;
+    accelerations.push(aFromDistance);
+  }
+
+  if (accelerations.length === 0) {
     return "impossible";
   }
 
-  const a = params.f / params.m;
-  return a;
+  return accelerations[0];
 }
 
+console.log(getAcceleration({ f: 10, m: 5 }));
 console.log(getAcceleration({ Δv: 100, Δt: 50 }));
+console.log(getAcceleration({ d: 10, t: 1, m: 5 }));
+console.log(getAcceleration({ m: 5 }));
