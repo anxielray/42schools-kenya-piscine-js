ae3f45275fda6f20317566d5ac72a0101154454c
anxielray <anxielworld@gmil.com>
2024-09-30 14:02:33 +0300

    [Add]:Add solution to the biggie and smalls

--- biggie-smalls.js
+++ biggie-smalls.js
@@ -1,3 +1,4 @@
-const smalls = -Number.MAX_VALUE;
-const biggie = Infinity;
+const smalls = -Number.MAX_VALUE//.toExponential().replace("+", "");
+const biggie = Number.MAX_VALUE.toExponential().replace("+", "");
 console.log(smalls);
+console.log(biggie);
