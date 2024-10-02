6d1582f131517de64beb6dda288a3257f5266f8d
anxielray <anxielworld@gmil.com>
2024-10-02 20:44:14 +0300

    [Add]:Add solution to the pyramid

--- pyramid.js
+++ pyramid.js
@@ -2,16 +2,16 @@ function pyramid(character, height) {
   let result = "";
   for (let i = 0; i < height; i++) {
     if (i === 0) {
-      let sp = " ".repeat(height);
+      let sp = " ".repeat(height-1);
       result += sp + character + "\n";
       continue;
     }
     let numCharacters = 1 + i * 2;
     let spaces = " ".repeat(height - i - 1);
-    let row = (character + " ").repeat(numCharacters).trim();
+    let row = (character).repeat(numCharacters).trim();
     result += spaces + row + "\n";
   }
   return result;
 }
 
-// console.log(pyramid("a", 5));
+console.log(pyramid("a", 5));
