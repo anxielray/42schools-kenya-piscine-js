1d57854796cb94ec9aeaaacd166d1d2de1557bb0
anxielray <anxielworld@gmil.com>
2024-10-02 15:45:18 +0300

    [Add]:Add solution to the the implementation of the split and the join functions

--- unbreakable.js
+++ unbreakable.js
@@ -10,7 +10,7 @@ function split(string, separator, limit) {
     for (let i = 0; i < string.length; i++) {
       result.push(string[i]);
     }
-    return removeEmptyString(result);
+    return result;
   }
 
   while (startIndex < string.length) {
@@ -31,7 +31,11 @@ function split(string, separator, limit) {
     }
   }
 
-  return removeEmptyString(result);
+  if (startIndex >= string.length && result[result.length - 1] !== "") {
+    result.push("");
+  }
+
+  return result;
 }
 
 function join(array, separator = ",") {
@@ -46,11 +50,4 @@ function join(array, separator = ",") {
   }
 
   return result;
-}
-
-function removeEmptyString(arr) {
-  if (arr.includes("")) {
-    return arr.filter((item) => item !== "");
-  }
-  return arr;
 }
