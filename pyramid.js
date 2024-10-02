e3cf02c38f034cfbc6ee16165f1ac0ef29033da2
anxielray <anxielworld@gmil.com>
2024-10-02 19:08:54 +0300

    [Add]:Add solution to the pyramid printting

--- pyramid.js
+++ pyramid.js
@@ -1,23 +1,24 @@
 function pyramid(str, height) {
-    if (height < 1) {
-      return "";
-    }else if (height === 1) {
-        return str;
-    }
-    if (str.length === 0) {
-      return "";
-    }
+  if (height < 1) {
+    return "";
+  } else if (height === 1) {
+    return str;
+  }
+  if (str.length === 0) {
+    return "";
+  }
+
   let result = "";
   for (let row = 1; row <= height; row++) {
     const spaces = " ".repeat(height - row);
-    str = str.repeat(2 * row - 1);
+    const characters = str.repeat(2 * row - 1);
     if (row !== height) {
-      result += spaces + str + "\n";
+      result += spaces + characters + "\n";
     } else {
-      result += spaces + str;
+      result += spaces + characters;
     }
   }
   return result;
 }
 
-console.log(pyramid("w", 10));
+// console.log(pyramid("a", 10));
