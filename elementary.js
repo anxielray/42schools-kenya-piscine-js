95ab20d354fb871a65cba3227195c310471adb1c
anxielray <anxielworld@gmil.com>
2024-10-02 10:11:15 +0300

    [Add]:Add solution to the elementary

--- elementary.js
+++ elementary.js
@@ -6,34 +6,39 @@
 */
 
 function multiply(a, b) {
-    let result = 0;
-    for (let i = 0; i < Math.abs(b); i++) {
-        result += a;
-    }
-    if (b < 0) {
-        result = -result;
-    }
-    return result;
+  let result = 0;
+  for (let i = 1; i <= Math.abs(b); i++) {
+    result += Math.abs(a);
+  }
+  if (b < 0) {
+    result = -result;
+  }
+  if (a < 0) {
+    result = -result;
+  }
+  return result;
 }
 
 function divide(a, b) {
-    if (b === 0) {
-        throw new Error("Cannot divide by zero");
-    }
-    let result = 0;
-    let sign = (a < 0 && b > 0) || (a > 0 && b < 0)? -1 : 1;
-    a = Math.abs(a);
-    b = Math.abs(b);
-    while (a >= b) {
-        result++;
-        a -= b;
-    }
-    return sign * result;
+  if (b === 0) {
+    throw new Error("Cannot divide by zero");
+  }
+  let result = 0;
+  let sign = (a < 0 && b > 0) || (a > 0 && b < 0) ? -1 : 1;
+  a = Math.abs(a);
+  b = Math.abs(b);
+  while (a >= b) {
+    result++;
+    a -= b;
+  }
+  return multiply(sign, result);
 }
 
 function modulo(a, b) {
-    if (b === 0) {
-        throw new Error("Cannot modulo by zero");
-    }
-    return a - (divide(a, b) * b);
+  if (b === 0) {
+    throw new Error("Cannot modulo by zero");
+  }
+  return a - multiply(divide(a, b), b);
 }
+
+console.log(multiply(-21, -4));
