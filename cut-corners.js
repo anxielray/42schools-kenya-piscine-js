b40d26cd41c6502a13de2093bf79e740cd2c7ac0
anxielray <anxielworld@gmil.com>
2024-10-02 14:40:50 +0300

    [Add]:Add solution to the implement the functions in the Math rounding functions

--- cut-corners.js
+++ cut-corners.js
@@ -4,109 +4,83 @@ function round(x) {
     sign = true;
     x = -1;
   }
-  let count = 0;//
-  while(!(x < 1 && x > -1)){
+  let count = 0;
+  while (!(x < 1 && x > -1)) {
     x -= 1;
     count++;
   }
-  if (x < 0.5){
+  if (x < 0.5) {
     if (sign) {
-        return -count;
-    }else {
-        return count;
-    }
-  }else {
-    if (sign) {
-        return -count - 1;
+      return -count;
     } else {
-        return count + 1;
+      return count;
+    }
+  } else {
+    if (sign) {
+      return -count - 1;
+    } else {
+      return count + 1;
     }
   }
 }
 
 function ceil(x) {
-  if (x === 0) {
-    return 0;
-  }
-  let integerPart = 0;
-
+  if (!x) return 0;
+  let sign = false;
+  let int = x;
   if (x < 0) {
-    for (let i = 0; i >= x; i--) {
-      integerPart = i;
-    }
-    return integerPart;
+    sign = true;
+    x = -x;
   }
-  if (x > 0) {
-    for (let i = 0; i <= x; i++) {
-      integerPart = i;
-    }
-    if (integerPart < x) {
-      return integerPart + 1;
-    }
+  while (!(int < 1 && int > -1)) {
+    int = -1;
+    count++;
   }
-  return integerPart;
+  if (sign) {
+    return -count;
+  } else {
+    return count + 1;
+  }
 }
 
 function floor(x) {
-  if (x === 0) {
-    return 0;
-  }
-  let integerPart = 0;
-
+  let sign = false;
   if (x < 0) {
-    for (let i = 0; i < -x; i++) {
-      integerPart = -i - 1;
-    }
-    return integerPart;
-  } else {
-    for (let i = 0; i <= x; i++) {
-      integerPart = i;
-    }
+    sign = true;
+    x = -1;
   }
-  return integerPart;
+  let int = x;
+  let count = 0;
+  while (!(int < 1 && int > -1)) {
+    int -= 1;
+    count++;
+  }
+  if (sign) {
+    return -count - 1;
+  } else {
+    return count;
+  }
 }
 
 function trunc(x) {
-  if (x === 0) {
-    return 0;
+  let count = 0;
+  if (x > 0xfffffffff) {
+    int -= 0xfffffffff;
+    count += 0xfffffffff;
   }
-  let integerPart = 0;
-
+  let sign = false;
   if (x < 0) {
-    for (let i = 0; i < -x; i++) {
-      integerPart = -i;
-    }
-    return integerPart;
+    sign = true;
+    x = -x;
+  }
+  let int = x;
+  while (!(int < 1 && int > -1)) {
+    int -= 1;
+    count++;
+  }
+  if (sign) {
+    return -count;
   } else {
-    for (let i = 0; i <= x; i++) {
-      integerPart = i;
-    }
+    return count;
   }
-  return integerPart;
-}
-
-function modulo(a, b) {
-  let preDivide = divide(a, b);
-  let postDivide = preDivide * b;
-  if (postDivide < 0) {
-    postDivide = -postDivide;
-    return a + postDivide;
-  }
-  return a - postDivide;
-}
-
-function divide(a, b) {
-  let result = 0;
-  let sign = (a < 0 && b > 0) || (a > 0 && b < 0) ? -1 : 1;
-  if (a < 0) {
-    a = -a;
-  }
-  if (b < 0) {
-    b = -b;
-  }
-  while (a >= b) {
-    result++;
-    a -= b;
-  }
-  return sign * result;
 }
