dd0246b1f55e7ad43cea88b652fa23a6701f3451
anxielray <anxielworld@gmil.com>
2024-10-02 13:42:16 +0300

    [Add]:Add solution to the math rounding implementations

--- cut-corners.js
+++ cut-corners.js
@@ -106,16 +106,3 @@ function divide(a, b) {
   }
   return sign * result;
 }
-
-const nums = [3, -3, 3, -3, 0]
-console.log(nums.map(round))
-console.log(nums.map(floor))
-console.log(nums.map(trunc))
-console.log(nums.map(ceil))
-
-// Output:
-
-// [ 4, -4, 3, -3 ]
-// [ 3, -4, 3, -4 ]
-// [ 3, -3, 3, -3 ]
-// [ 4, -3, 4, -3 ]
