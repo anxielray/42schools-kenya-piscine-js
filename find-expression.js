a27b218386d3f025e021222551b333e0d9f1617e
anxielray <anxielworld@gmil.com>
2024-10-02 23:04:05 +0300

    [Add]:Add solution to the find expression

--- find-expression.js
+++ find-expression.js
@@ -5,24 +5,22 @@
     If the number can not be reached you should return undefined
 
 For example, for the number 8, you must first multiply by 2 twice, and then add 4. Your function should return something like: 1 *2 *2 +4.*/
-const add4 = 4;
-const mul2 = 2;
 function findExpression(number) {
-    for (let i = 0; i < 100000; i++) {
-        let result = 1;
-        let sequence = "1";
-        while(result <= number) {
-            if (result === number) {
-                return sequence;
-            }
-            if (Math.random() < 0.4 + 0.1){
-                result += add4;
-                sequence += " "+ add4;//` + ${add4}`;
-            }else {
-                result *= mul2;
-                sequence += " *"+ mul2;//` * ${mul2}`;
-            }
-        }
+  for (let i = 0; i < 100000; i++) {
+    let result = 1;
+    let sequence = "1";
+    while (result <= number) {
+      if (result === number) {
+        return sequence;
+      }
+      if (Math.random() < 0.4 + 0.1) {
+        result += add4;
+        sequence += " " + add4; //` + ${add4}`;
+      } else {
+        result *= mul2;
+        sequence += " *" + mul2; //` * ${mul2}`;
+      }
     }
-return undefined;
+  }
+  return undefined;
 }
