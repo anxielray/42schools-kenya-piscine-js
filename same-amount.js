a32a21bcf28869562229838d1c4a198e76a468ca
anxielray <anxielworld@gmil.com>
2024-10-04 10:40:55 +0300

    [Add]:Add solution to the same amount of existence in strings

--- same-amount.js
+++ same-amount.js
@@ -6,12 +6,16 @@ function sameAmount(string, regex1, regex2) {
   const matches1 = string.match(regex1);
   const matches2 = string.match(regex2);
 
-  if (!matches1 || !matches2) {
+  if (!matches1 && matches2) {
     return false;
+  } else if (matches1 && !matches2) {
+    return false;
+  } else if (!matches1 && !matches2) {
+    return true;
   }
 
   return matches1.length === matches2.length;
 }
 
 // const data = "quick brown fox jumps over the lazy dog";
-// console.log(sameAmount(data, /q /, /qqqqqqq/));
+// console.log(sameAmount("data", /q /, /qqqqqqq/));
