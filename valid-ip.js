155f875b949f0d953940b6681921ca4974043972
anxielray <anxielworld@gmil.com>
2024-10-04 17:43:34 +0300

    [Add]:Add solution to the valid-ip

--- valid-ip.js
+++ valid-ip.js
@@ -1,20 +1,23 @@
-function findIP(dataSet) {
-  const ipRegex =
-    /(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(?::(6553[0-5]|655[0-2][0-9]|6[0-4][0-9]{3}|[0-5]?[0-9]{1,4}))?/g;
+function findIP(str) {
+  const regex =
+    /\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(:[0-9]{1,5})?\b/g;
 
-  const matches = dataSet.match(ipRegex);
+  const matches = str.match(regex);
 
-  return matches || [];
+  return matches
+    ? matches.filter((ip) => {
+        const portMatch = ip.match(/:(\d{1,5})$/);
+        if (portMatch) {
+          const port = parseInt(portMatch[1], 10);
+          return port >= 0 && port <= 65535;
+        }
+        return true;
+      })
+    : [];
 }
 
-// const inputString = `
-// Here are some IP addresses:
-// 192.168.1.1:8080
-// 10.0.0.256 // Invalid IP
-// 172.16.254.1
-// 255.255.255.255:65535
-// 192.168.01.1 // Invalid due to leading zero
-// `;
-
-// console.log(findIP(inputString));
-// Output: [ '192.168.1.1:8080', '172.16.254.1', '255.255.255.255:65535' ]
+console.log(
+  findIP(
+    "These are the IPs: 192.168.0.1, 256.256.256.256, 10.0.0.1:8080, 172.16.254.1:65536, 172.16.254.1:65000."
+  )
+);
