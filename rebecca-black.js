2e283095689a9cebefa5354c643f0c76d8be5971
anxielray <anxielworld@gmil.com>
2024-10-07 15:48:00 +0300

    [Add]:Add solution to the rebecca's games for the year

--- rebecca-black.js
+++ rebecca-black.js
@@ -17,13 +17,55 @@ function isWeekend(date) {
 }
 
 function isLeapYear(date) {
-    const year = date.getFullYear();
+  const year = new Date(date).getFullYear();
   return year % 4 === 0;
 }
 
 function isLastDayOfMonth(date) {
-    const nextDate = new Date(date.getFullYear(), date.getMonth() + 1, 1);
-  return nextDate.getDate() === date.getDate();
+  if (!isLeapYear(date)) {
+    if (new Date(date).getMonth() !== 1) {
+      if (
+        new Date(date).getMonth() !== 0 &&
+        new Date(date).getMonth() !== 4 &&
+        new Date(date).getMonth() !== 6 &&
+        new Date(date).getMonth() !== 9 &&
+        new Date(date).getMonth() !== 11
+      ) {
+        return new Date(date).getDate() === 30;
+      } else if (
+        new Date(date).getMonth() !== 3 &&
+        new Date(date).getMonth() !== 5 &&
+        new Date(date).getMonth() !== 8 &&
+        new Date(date).getMonth() !== 10
+      ) {
+        return new Date(date).getDate() === 31;
+      }
+    } else {
+      return new Date(date).getDate() === 28;
+    }
+  } else {
+    if (new Date(date).getMonth() !== 1) {
+      if (
+        new Date(date).getMonth() !== 0 &&
+        new Date(date).getMonth() !== 2 &&
+        new Date(date).getMonth() !== 4 &&
+        new Date(date).getMonth() !== 6 &&
+        new Date(date).getMonth() !== 9 &&
+        new Date(date).getMonth() !== 11
+      ) {
+        return new Date(date).getDate() === 30;
+      } else {
+        return new Date(date).getDate() === 31;
+      }
+    } else {
+      if (new Date(date).getDate() > 29) {
+        return false;
+      }
+      return new Date(date).getDate() === 29;
+    }
+  }
 }
 // console.log(isFriday(new Date('2014-08-29')));
 // console.log(isLeapYear(new Date('2004-08-29')));
+// console.log(isLastDayOfMonth("2020-02-31"));
+// console.log(new Date("2020-02-20").getMonth());
