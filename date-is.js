6e83c9763d48f459b3ce52b21931b925e302f013
anxielray <anxielworld@gmil.com>
2024-10-07 11:43:36 +0300

    [Add]:Add solution to the validation of the dates

--- date-is.js
+++ date-is.js
@@ -55,4 +55,5 @@ function isValid(date) {
     return true;
   }
 }
-console.log(isValid(Date.now()));
+console.log( isValid(new Date('1995-12-17T03:24:00').getTime()))
+// console.log(isValid(Date.now()));
