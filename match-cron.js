e951d4b28d367f1defc0c0538f2222d88b18485f
anxielray <anxielworld@gmail.com>
2024-10-07 21:15:34 +0300

    [Add]:Add solution to the match-cron

--- match-cron.js
+++ match-cron.js
@@ -1,84 +1,36 @@
-/* Create a function named matchCron, which accepts a valid cron string, and a valid Date. Your function should return true if the pattern matches the Date.
-
-    You only need to implement numbers and *. Other complex patterns are not required.
-
-Only valid patterns will be tested.
-Example
-
-matchCron('9 * * * *', new Date('2020-05-30 18:09:00')) // -> true
-matchCron('9 * * * *', new Date('2020-05-30 19:09:00')) // -> true
-matchCron('9 * * * *', new Date('2020-05-30 19:21:00')) // -> false
-         | | | | |
-         | | | | +- Day of the Week   (range: 1-7, 1 is Monday)
-         | | | +--- Month of the Year (range: 1-12)
-         | | +----- Day of the Month  (range: 1-31)
-         | +------- Hour              (range: 0-23)
-         +--------- Minute            (range: 0-59)
- */
-
-function matchCron(cron, date) {
-  const days = {
-    0: 7,
-    1: 1,
-    2: 2,
-    3: 3,
-    4: 4,
-    5: 5,
-    6: 6,
-  };
-  if (/^[^0-9*]/.test(cron)) {
-    return false;
-  }
-  if (cron.length !== 5) {
-    return false;
-  }
-  const cronSplit = cron.split(" ");
-  for (let i = 0; i < cronSplit.length; i++) {
-    if (cronSplit[i] !== "*") {
-      let num = parseInt(cron[0], 10);
-      if (num < 60 && num > -1) {
-        if (num === date.getMinutes()) {
-          if (hour !== "*") {
-            let num = parseInt(cron[1], 10);
-            if (num === date.getHours()) {
-              if (dayMonth !== "*") {
-                let num = parseInt(cron[2], 10);
-                if (num === date.getDate()) {
-                  if (month !== "*") {
-                    let num = parseInt(cron[3], 10);
-                    if (num === date.getMonth() + 1) {
-                      if (weekday !== "*") {
-                        let num = parseInt(cron[4], 10);
-                        if (num === days[date.getDay()]) {
-                          return true;
-                        } else {
-                          false;
-                        }
-                      } else {
-                       continue;
-                      }
-                    }
-                  } else {
-                    continue;
-                  }
-                }
-              }else {
-                continue;
-              }
-            }
-          }else {
-            continue;
-          }
-        } else {
-          return false;
+function matchCron(inputCron, date) {
+    let cron = {};
+    inputCron.split(" ").forEach((part, i) => {
+        if (part === "*") return;
+        switch (i) {
+            case 0:
+                cron.minute = part;
+                break;
+            case 1:
+                cron.hour = part;
+                break;
+            case 2:
+                cron.date = part;
+                break;
+            case 3:
+                cron.month = part;
+                break;
+            case 4:
+                cron.day = part;
+                break;
         }
-      } else {
-        return false;
-      }
-    }else{
-        continue;
+    });
+    date = {
+        minute: date.getMinutes(),
+        hour: date.getHours(),
+        date: date.getDate(),
+        month: date.getMonth() + 1,
+        day: date.getDay(),
+    };
+    for (let key in cron) {
+        if (cron[key] !== date[key].toString()) {
+            return false;
+        }
     }
-  }
+    return true;
 }
-
-console.log(matchCron("9 * * * *", new Date("2020-05-30 18:09:00")));
