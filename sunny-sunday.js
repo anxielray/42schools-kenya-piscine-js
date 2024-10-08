0e5250f927de77d7efbfdd3577f2d600115869f2
anxielray <anxielworld@gmail.com>
2024-10-08 09:22:13 +0300

    [Add]:Add solution to the sunny-sunday

--- sunny-sunday.js
+++ sunny-sunday.js
@@ -1,38 +1,11 @@
 function sunnySunday(date) {
-    const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let dayOfWeek = date.getDay();
 
-    function isLeapYear(year) {
-        if (year % 4 === 0) {
-            if (year % 100 === 0) {
-                return year % 400 === 0;
-            }
-            return true;
-        }
-        return false;
+    if (dayOfWeek === 0) {
+        date.setDate(date.getDate() + 1);
+        dayOfWeek = 1;
     }
 
-    function daysInYear(year) {
-        return isLeapYear(year) ? 366 : 365;
-    }
-
-    let totalDays = 0;
-
-    for (let year = 1; year < date.getFullYear(); year++) {
-        totalDays += daysInYear(year);
-    }
-
-    const daysInMonth = [31, isLeapYear(date.getFullYear()) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    for (let month = 0; month < date.getMonth(); month++) {
-        totalDays += daysInMonth[month];
-    }
-    totalDays += date.getDate() - 1;
-
-    const sundaysSkipped = Math.floor(totalDays / 7);
-    const adjustedDays = totalDays - sundaysSkipped;
-    const weekdayIndex = adjustedDays % 5;
-
-    return weekdays[weekdayIndex];
+    let weekdayStrings = ["", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    return weekdayStrings[dayOfWeek];
 }
-
-console.log(sunnySunday(new Date('0001-01-02')))
-
