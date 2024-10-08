f8fc8270ad2648fe5c81928d1b1ef18dae29290d
anxielray <anxielworld@gmail.com>
2024-10-08 07:26:11 +0300

    [Add]:Add solution to the counting-em

--- count-leap-years.js
+++ count-leap-years.js
@@ -1,12 +1,12 @@
-function countLeapYears(date){
+eeasalfunction countLeapYears(date){
 let yrs = 0;
 for (let i = 1; i < date.getFullYear(); i++){
 
-if ( i % 4 === 0 && (i  && 100 != 0 || i % 400 === 0)) {
+if ( i % 4 === 0) {
 yrs++;
 }
 
 }
-return;
+return yrs;
 }
 
