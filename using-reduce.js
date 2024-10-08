ef401ddf4fc9f8466f172a213d55bffdd444b669
anxielray <anxielworld@gmil.com>
2024-10-08 14:56:34 +0300

    [Add]:Add solution to the using the reduce method of arays

---
+++ using-reduce.js
@@ -0,0 +1,26 @@
+/* Create functions : 
+
+    adder: accepts an array of numbers, and returns the sum as a number.
+
+    sumOrMul: accepts an array of numbers and adds or multiplies its elements depending on whether the element is odd or even. Even = multiply. Odd = add.
+
+    funcExec: accepts an array of functions and executes them using reduce, returning the result.
+*/
+
+function adder(arr) {
+  return arr.reduce((acc, curr) => acc + curr, 0);
+}
+
+function sumOrMul(arr) {
+  return arr.reduce(
+    (acc, curr) => (curr % 2 === 0 ? acc * curr : acc + curr),
+    0
+  );
+}
+
+function funcExec(arr) {
+  return arr.reduce(
+    (acc, curr) => acc.apply(null, curr),
+    Function.prototype.call
+  );
+}
