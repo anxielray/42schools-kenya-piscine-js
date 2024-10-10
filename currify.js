b59740a8e931d6746440585338f05736206f3cff
anxielray <anxielworld@gmil.com>
2024-10-10 23:01:23 +0300

    [Dele

--- currify.js
+++
@@ -1,7 +0,0 @@
-function currify(func) {
-    return function currified(...args) {
-        return args.length >= func.length
-            ? func(...args)
-            : currified.bind(null, ...args);
-    };
-}
