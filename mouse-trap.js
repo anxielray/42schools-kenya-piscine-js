c3f4b4f18af0bf0ee5190b3e95d75d95d49f2b19
anxielray <anxielworld@gmil.com>
2024-10-11 01:01:13 +0300

    [Add]:Add solution to the Mouse trap in Java Script

--- mouse-trap.js
+++ mouse-trap.js
@@ -1,109 +1,113 @@
 var circles = [];
 var box;
 class Circle {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.diameter = 51;
-    this.isTrapped = false;
-    this.HTML = null;
-    this.draw();
-    circles.push(this);
-  }
-  draw() {
-    this.HTML = document.createElement("div");
-    this.HTML.classList.add("circle");
-    this.HTML.style.position = "absolute";
-    this.HTML.style.top = this.y + "px";
-    this.HTML.style.left = this.x + "px";
-    this.HTML.style.background = "white";
-    this.trapped();
-    document.body.appendChild(this.HTML);
-  }
-  move(x, y) {
-    this.trapped();
-    if (!this.isTrapped) {
-      this.x = x;
-      this.y = y;
-      this.HTML.style.top = this.y + "px";
-      this.HTML.style.left = this.x + "px";
-    } else {
-      if (this.inRectangle(x, y)) {
+    constructor(x, y) {
         this.x = x;
         this.y = y;
+        this.diameter = 51;
+        this.isTrapped = false;
+        this.HTML = null;
+        this.draw();
+        circles.push(this);
+    }
+    draw() {
+        this.HTML = document.createElement("div");
+        this.HTML.classList.add("circle");
+        this.HTML.style.position = "absolute";
         this.HTML.style.top = this.y + "px";
         this.HTML.style.left = this.x + "px";
-      } else {
-        if (this.inRectangle(x, this.y)) {
-          this.x = x;
-          this.HTML.style.left = this.x + "px";
-        } else if (this.inRectangle(this.x, y)) {
-          this.y = y;
-          this.HTML.style.top = this.y + "px";
+        this.HTML.style.background = "white";
+        this.trapped();
+        document.body.appendChild(this.HTML);
+    }
+    move(x, y) {
+        this.trapped();
+        if (!this.isTrapped) {
+            this.x = x;
+            this.y = y;
+            this.HTML.style.top = this.y + "px";
+            this.HTML.style.left = this.x + "px";
+        } else {
+            if (this.inRectangle(x, y)) {
+                this.x = x;
+                this.y = y;
+                this.HTML.style.top = this.y + "px";
+                this.HTML.style.left = this.x + "px";
+            } else {
+                if (this.inRectangle(x, this.y)) {
+                    this.x = x;
+                    this.HTML.style.left = this.x + "px";
+                } else if (this.inRectangle(this.x, y)) {
+                    this.y = y;
+                    this.HTML.style.top = this.y + "px";
+                }
+            }
         }
-      }
     }
-  }
-  trapped() {
-    if (
-      this.x + this.diameter / 2 < box.width &&
-      this.x - this.diameter / 2 < box.width &&
-      this.y - this.diameter / 2 > box.height &&
-      this.y + this.diameter / 2 < box.height
-    ) {
-      this.isTrapped = true;
-      this.HTML.style.background = "var(--purple)";
-    } else {
-      this.isTrapped = false;
-      this.HTML.style.background = "white";
+    trapped() {
+        if (
+            this.x > box.x &&
+            this.x + this.diameter < box.x + box.width &&
+            this.y > box.y &&
+            this.y + this.diameter < box.y + box.height
+        ) {
+            this.isTrapped = true;
+            this.HTML.style.background = "var(--purple)";
+        } else {
+            this.isTrapped = false;
+            this.HTML.style.background = "white";
+        }
     }
-  }
-  inRectangle(x, y) {
-    return (
-      x + this.diameter / 2 < box.width &&
-      x - this.diameter / 2 < box.width &&
-      y - this.diameter / 2 > box.height &&
-      y + this.diameter / 2 < box.height
-    );
-  }
+    inRectangle(x, y) {
+        if (
+            x > box.x &&
+            x + this.diameter < box.x + box.width &&
+            y > box.y+1 &&
+            y + this.diameter < box.y + box.height
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
 class Box {
-  constructor() {
-    this.HTML = document.createElement("div");
-    this.HTML.classList.add("box");
-    this.HTML.style.position = "absolute";
-    this.HTML.style.top = "50%";
-    this.HTML.style.left = "50%";
-    this.HTML.style.transform = "translate(-50%, -50%)";
-    document.body.appendChild(this.HTML);
-    this.x = this.HTML.offsetLeft - this.HTML.offsetWidth / 2 - 1; // -1 to account for the border
-    this.y = this.HTML.offsetTop - this.HTML.offsetHeight / 2 - 1;
-    this.width = this.HTML.offsetWidth + 1; // +1 to account for the border
-    this.height = this.HTML.offsetHeight + 1;
-  }
+    constructor() {
+        this.HTML = document.createElement("div");
+        this.HTML.classList.add("box");
+        this.HTML.style.position = "absolute";
+        this.HTML.style.top = "50%";
+        this.HTML.style.left = "50%";
+        this.HTML.style.transform = "translate(-50%, -50%)";
+        document.body.appendChild(this.HTML);
+        this.x = this.HTML.offsetLeft - this.HTML.offsetWidth / 2 - 1; // -1 to account for the border
+        this.y = this.HTML.offsetTop - this.HTML.offsetHeight / 2 - 1;
+        this.width = this.HTML.offsetWidth + 1; // +1 to account for the border
+        this.height = this.HTML.offsetHeight + 1;
+    }
 }
 
 document.body.addEventListener("click", (e) => {
-  createCircle(e);
+    createCircle(e);
 });
 
 document.body.addEventListener("mousemove", (e) => {
-  moveCircle(e);
+    moveCircle(e);
 });
 
 function createCircle(e) {
-  if (e === undefined) return;
-  new Circle(e.clientX - 25, e.clientY - 25);
+    if (e === undefined) return;
+    new Circle(e.clientX - 25, e.clientY - 25);
 }
 
 function moveCircle(e) {
-  if (e === undefined || circles.length === 0) return;
-  circles[circles.length - 1].move(e.clientX - 25, e.clientY - 25);
+    if (e === undefined || circles.length === 0) return;
+    circles[circles.length - 1].move(e.clientX - 25, e.clientY - 25);
 }
 
 function setBox() {
-  box = new Box();
+    box = new Box();
 }
 
 export { createCircle, moveCircle, setBox };
