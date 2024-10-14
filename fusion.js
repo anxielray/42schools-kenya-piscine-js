20141188e5224d127a0ca0d17c6069545823e419
anxielray <anxielworld@gmil.com>
2024-10-14 13:14:25 +0300

    [Add]:Add solution to the fusing the objects

--- fusion.js
+++ fusion.js
@@ -1,31 +1,35 @@
-function fusion(obj, obj2) {
-  const result = {};
-
-  const keys = new Set([...Object.keys(obj), ...Object.keys(obj2)]);
-
-  for (let key of keys) {
-    const value1 = obj[key];
-    const value2 = obj2[key];
-
-    if (Array.isArray(value1)) {
-      result[key] = value1.concat(value2 || []);
-    } else if (Array.isArray(value2)) {
-      result[key] = (value1 || []).concat(value2);
-    } else if (typeof value1 === "string") {
-      result[key] = value1 + " " + value2;
-    } else if (typeof value1 === "number") {
-      result[key] = (value1 || 0) + (value2 || 0);
-    } else if (typeof value2 === "number") {
-      result[key] = (value1 || 0) + value2;
-    } else if (typeof value1 === "object" && value1 !== null) {
-      result[key] = fusion(value1, value2 || {});
-    } else if (typeof value2 === "object" && value2 !== null) {
-      result[key] = fusion({}, value2);
+function fusion(obj1, obj2) {
+  var fusioned = {};
+  for (var key in obj1) {
+    if (!obj1.hasOwnProperty(key)) continue;
+    if (obj2.hasOwnProperty(key)) {
+      if (is.obj(obj1[key]) && is.obj(obj2[key])) {
+        fusioned[key] = fusion(obj1[key], obj2[key]);
+      } else if (is.arr(obj1[key]) && is.arr(obj2[key])) {
+        fusioned[key] = obj1[key].concat(obj2[key]);
+      } else if (is.num(obj1[key]) && is.num(obj2[key])) {
+        fusioned[key] = obj1[key] + obj2[key];
+      } else if (is.str(obj1[key]) && is.str(obj2[key])) {
+        fusioned[key] = obj1[key] + " " + obj2[key];
+      } else {
+        fusioned[key] = obj2[key];
+      }
     } else {
-      result[key] = value2 !== undefined ? value2 : value1;
+      fusioned[key] = obj1[key];
     }
   }
-
-  return result;
+  for (var key in obj2) {
+    if (!obj2.hasOwnProperty(key)) continue;
+    if (!obj1.hasOwnProperty(key)) {
+      fusioned[key] = obj2[key];
+    }
+  }
+  return fusioned;
 }
-console.log(fusion({ str: 'hello' }, { str: '' }));
+
+const is = {};
+is.num = (n) => typeof n === "number";
+is.str = (n) => typeof n === "string";
+is.arr = (n) => Array.isArray(n);
+is.obj = (n) => typeof n === "object" && !is.fun(n) && !is.arr(n) && n !== null;
+is.fun = (n) => typeof n === "function";
