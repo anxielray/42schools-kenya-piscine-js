ff827c40c33cfde6a8528d5e98b1453e170a6f8b
anxielray <anxielworld@gmil.com>
2024-10-16 14:38:04 +0300

    [Add]:Add solution to the Throttl

---
+++ throttle.js
@@ -0,0 +1,47 @@
+/* Create two functions that will work like _.throttle from lodash.
+
+    throttle: don't worry about the options.
+    opThrottle: implement the trailing and leading options.
+*/
+function throttle(func, limit) {
+  let lastCall = 0;
+
+  return function (...args) {
+    const context = this;
+    const now = Date.now();
+
+    if (now - lastCall >= limit) {
+      lastCall = now;
+      func.apply(context, args);
+    }
+  };
+}
+
+function opThrottle(func, limit, options = {}) {
+  let lastCall = 0;
+  let timeout;
+
+  return function (...args) {
+    const context = this;
+    const now = Date.now();
+
+    if (!lastCall && options.leading !== false) {
+      func.apply(context, args);
+    }
+
+    if (now - lastCall >= limit) {
+      lastCall = now;
+      if (timeout) {
+        clearTimeout(timeout);
+        timeout = null;
+      }
+      func.apply(context, args);
+    } else if (options.trailing !== false) {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        lastCall = 0;
+        func.apply(context, args);
+      }, limit - (now - lastCall));
+    }
+  };
+}
