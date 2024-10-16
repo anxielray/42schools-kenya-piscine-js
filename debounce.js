7fa6de9f62e1205bad3f64cd9f88c4b51e99a636
anxielray <anxielworld@gmil.com>
2024-10-16 13:10:29 +0300

    [Add]:Add solution to the debounce with and without options

---
+++ debounce.js
@@ -0,0 +1,52 @@
+/* 
+Create two functions that will work like _.debounce from lodash.
+
+    debounce: don't worry about the options.
+    opDebounce: implement the leading options.
+ */
+
+function debounce(func, wait) {
+  let timeout;
+
+  return function (...args) {
+    const context = this;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(context, args), wait);
+  };
+}
+
+// const logMessage = debounce(() => console.log("Debounced!"), 1000);
+
+// logMessage();
+
+function debounceWithTrailing(
+  func,
+  wait,
+  trailing = true,
+  leading = false,
+  maxWeight = 1
+) {
+  let timeout;
+
+  return function (...args) {
+    const context = this;
+    const callNow = !timeout;
+
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      timeout = null;
+      if (trailing) func.apply(context, args);
+    }, wait);
+
+    if (callNow && !trailing) {
+      func.apply(context, args);
+    }
+  };
+}
+
+const logMessageWithTrailing = debounceWithTrailing(
+  () => console.log("Debounced with trailing!"),
+  1000,
+  true
+);
+logMessageWithTrailing();
